add3250eada386c6572f84453be28aae3d80f434
Emmanuel Gonzalez <emm.gs17@gmail.com>
2022-02-02 23:43:32 -0600

    Add input verification and get winner methods

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -24,11 +24,37 @@ ___ ___ ___
   end
 
   def write_slot(place, mark)
-    @slots[place] = mark unless @slots[place]
+    occupied = @slots[place]
+    @slots[place] = mark unless occupied
+  end
+
+  def slot_occupied?(place)
+    @slots[place]
   end
 
   def full?
     @slots.values.none?(nil)
+  end
+
+  def find_three
+    find_three_in_row || find_three_in_col || find_three_in_diag
+  end
+
+  def find_three_in_row
+    (@slots[1] == @slots[2] && @slots[1] == @slots[3]) ||
+      (@slots[4] == @slots[5] && @slots[4] == @slots[6]) ||
+      (@slots[7] == @slots[8] && @slots[7] == @slots[9])
+  end
+
+  def find_three_in_col
+    (@slots[1] == @slots[4] && @slots[1] == @slots[7]) ||
+      (@slots[2] == @slots[5] && @slots[2] == @slots[8]) ||
+      (@slots[3] == @slots[6] && @slots[3] == @slots[9])
+  end
+
+  def find_three_in_diag
+    (@slots[1] == @slots[5] && @slots[1] == @slots[9]) ||
+      (@slots[3] == @slots[5] && @slots[3] == @slots[7])
   end
 end
 
@@ -43,6 +69,7 @@ end
 
 class Game
   def initialize
+    @current_turn = 0
     @board = Board.new
     @players = [Player.new('X', 'Player 1'), Player.new('O', 'Player 2')]
     @current_player = @players[0]
@@ -52,6 +79,7 @@ class Game
   private
 
   def turn
+    @current_turn += 1
     selected_slot = player_input
     @board.write_slot(selected_slot, @current_player.mark)
     @board.print_board
@@ -59,18 +87,33 @@ class Game
   end
 
   def player_input
-    puts "#{@current_player.name}, select a free slot"
-    gets.to_i
+    valid_slot = false
+    input = 0
+    until valid_slot
+      puts "#{@current_player.name}, select a free slot"
+      input = gets.to_i
+      valid_slot = !@board.slot_occupied?(input)
+    end
+    input
   end
 
   def swap_players
     @current_player, @next_player = @next_player, @current_player
   end
 
+  def winner?
+    @board.find_three if @current_turn > 4
+  end
+
   public
 
   def play
     @board.print_board
-    turn until @board.full?
+    turn until @board.full? || winner?
+    if winner?
+      puts "#{@next_player.name} won the game"
+    else
+      puts 'It\'s a draw'
+    end
   end
 end
